6e3d8d1a0e64f09238224287822951100f69d146
FAtimaEzzahraaAbbari <fatimazahraa.Abbari@gmail.com>
2023-10-05 15:13:57 -0700

    puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Description: 'A c program that prints a line to the standard error
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 return (1);
 }
