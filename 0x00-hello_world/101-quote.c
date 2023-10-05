b24dcf8eb8a643022ca40dd3af4868b17d06ce9d
FAtimaEzzahraaAbbari <fatimazahraa.Abbari@gmail.com>
2023-10-05 15:26:20 -0700

    no chmod

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Description: 'A c program that prints a line to the standard error
+ * Description:'Aprogram that prints a line to the standard erroe
  * Return: Always 1 (Success)
  */
 int main(void)
@@ -10,3 +10,4 @@ int main(void)
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19 ", stdout);
 return (1);
 }
+
