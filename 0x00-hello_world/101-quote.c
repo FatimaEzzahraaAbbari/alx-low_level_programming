d4ed62f0f54f096c6acb9f2f9f975d54cc54a0b6
FAtimaEzzahraaAbbari <fatimazahraa.Abbari@gmail.com>
2023-10-05 15:45:56 -0700

    i

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,7 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19 ", stderr);
 	return (1);
 }
 
