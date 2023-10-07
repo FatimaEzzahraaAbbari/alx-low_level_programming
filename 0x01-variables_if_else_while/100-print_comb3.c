9c41b450b538d69abd2cd6fa74eadc81d233b614
FAtimaEzzahraaAbbari <fatimazahraa.Abbari@gmail.com>
2023-10-06 18:58:57 -0700

    omg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,14 +5,14 @@
  */
 int main(void)
 {
-	int ones = '0';
-	int tens = '0';
+	int ones;
+	int tens;
 
 	for (tens = '0'; tens <= '9'; tens++)
 	{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
-			if (!(ones == tens) || (tens > ones))
+			if (ones != tens && tens < ones)
 			{
 				putchar(tens);
 				putchar(ones);
