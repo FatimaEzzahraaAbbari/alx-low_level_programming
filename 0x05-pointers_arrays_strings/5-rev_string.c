fa4a7ee43b376bd56f6199a44a75d04d8cfe4d74
FAtimaEzzahraaAbbari <fatimazahraa.Abbari@gmail.com>
2023-10-16 15:59:40 -0700

    8

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,30 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse, followed by a new line
- * @s: string to be printed
+ * rev_string - reverse a string.
+ *
+ * @s: the string to be reversed.
  */
-void print_rev(char *s)
-{
-	int i, j, len;
 
+void rev_string(char *s)
+{
+	int length, j, i;
+	char v1, v2;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+
+	j = length - 1;
 	i = 0;
 
-	while (s[i] != '\0')
+	while (j > i)
 	{
+		v1 = s[i];
+		v2 = s[j];
+		s[i] = v2;
+		s[j] = v1;
+		j--;
 		i++;
 	}
-
-	len = i;
-
-	for (j = len - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
-
-	_putchar('\n');
 }
