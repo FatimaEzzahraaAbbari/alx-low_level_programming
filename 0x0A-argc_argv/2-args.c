da4721e32d0df1bb5059de44675854ee2e789fb8
FAtimaEzzahraaAbbari <fatimazahraa.Abbari@gmail.com>
2023-10-27 13:27:55 -0700

    o

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+
 
 /**
  * main - prints all arguments it receives
@@ -9,10 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	i = 0;
-	for (; i < argc ; i++)
+	for (int i = 0; i < argc ; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
