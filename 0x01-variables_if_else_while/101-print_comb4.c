aee5caa38538a7c88fb288b8ed24377e693cd0f2
FatimaEzzahraaAbbari <142620522+FatimaEzzahraaAbbari@users.noreply.github.com>
2024-03-31 22:16:18 +0000

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,42 +1,27 @@
 #include <stdio.h>
+#include <stdint.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+ * Main - prints
+ * Return : Always 0 (success)
+*/
 int main(void)
 {
-	int digit1 = 0;
-	int digit2, digit3;
+        int i, j, k;
 
-	while (digit1 <= 9)
-	{
-		digit2 = 0;
-		while (digit2 <= 9)
-		{
-			digit3 = 0;
-			while (digit3 <= 9)
-			{
-				if (digit1 != digit2 &&
-						digit1 < digit2 &&
-						digit2 != digit3 &&
-						digit2 < digit3)
-				{
-					putchar(digit1 + 48);
-					putchar(digit2 + 48);
-					putchar(digit3 + 48);
-
-					if (digit1 + digit2 + digit3 != 24)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				digit3++;
-			}
-			digit2++;
-		}
-		digit1++;
-	}
-	putchar('\n');
-	return (0);
+        for (i = 0; i < 10; i++)
+        {
+                for (j = i + 1; j < 10; j++)
+                {
+                        for (k = j + 1; k < 10; k++)
+                        {
+                                putchar(i + '0');
+                                putchar(j + '0');
+                                putchar(k + '0');
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
